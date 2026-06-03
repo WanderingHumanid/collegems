5a60d9ca80e461fb2d142eba43f6123c4b49ec54
gous <gousbahurupi5@gmail.com>
2026-06-03 17:25:09 +0530

    fix: add missing Library import in App.tsx

--- collegems-client/src/App.tsx
+++ collegems-client/src/App.tsx
@@ -14,6 +14,7 @@ import StudentResults from "./user-components/StudentResults";
 import EventsStudent from "./user-components/EventsStudent";
 import QuickAccessAll from "./pages/QuickAccessAll";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
+import Library from "./common-components-management/Library";
 export default function App() {
   return (
     <BrowserRouter>
