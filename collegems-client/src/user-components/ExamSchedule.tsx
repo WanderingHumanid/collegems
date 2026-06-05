0e8a1b47b6cc66b999c16868e91f66bcba41c76d
imanchalsingh <singhanchal159357@gmail.com>
2026-06-05 09:38:36 +0530

    feat: add theme context import to ExamSchedule component for styling support

--- collegems-client/src/user-components/ExamSchedule.tsx
+++ collegems-client/src/user-components/ExamSchedule.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useTheme } from "../context/ThemeContext";
 import {
   Calendar, Clock, MapPin, BookOpen, Search, RefreshCw,
   Filter, ChevronDown, Download, Plus, Building2, GraduationCap,
